b58302cfdc5e8f586ad3df0f6155df8cec736851
Meduza3 <jellyfish200325@gmail.com>
2023-10-22 17:37:29 +0200

    Added check for windows focus when running useinputs

--- main.cpp
+++ main.cpp
@@ -25,24 +25,25 @@ void eventHandler(sf::RenderWindow& window, bool& isRunning) {
     }
 }
 
-void userInputHandler(Player& player, USEinput& button0, USEinput& button1) {
+void userInputHandler(Player& player, USEinput& button0, USEinput& button1, sf::RenderWindow& window) {
     auto lastRunTime = std::chrono::high_resolution_clock::now();
 
     while(true){
         auto currentTime = std::chrono::high_resolution_clock::now();
-
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num0)){
-            auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastRunTime).count();
-            if(elapsedTime >= 15){
-                button0.run();
-                lastRunTime = currentTime;
-            }
-        } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num1)){
-            auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastRunTime).count();
-            if(elapsedTime >= 15){
-                button1.run();
-                lastRunTime = currentTime;
-            }
+        if(window.hasFocus()){
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num0)){
+                auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastRunTime).count();
+                if(elapsedTime >= 15){
+                    button0.run();
+                    lastRunTime = currentTime;
+                }
+            } else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Num1)){
+                auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentTime - lastRunTime).count();
+                if(elapsedTime >= 15){
+                    button1.run();
+                    lastRunTime = currentTime;
+                }
+            }   
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(20)); // Add a small delay to avoid high CPU usage
@@ -121,7 +122,7 @@ int main () {
     std::cout << BOLD << "Fight!" << RESET << std::endl;
 
     std::thread eventThread(eventHandler, std::ref(window), std::ref(isRunning));
-    std::thread inputThread(userInputHandler, std::ref(player), std::ref(button0), std::ref(button1));
+    std::thread inputThread(userInputHandler, std::ref(player), std::ref(button0), std::ref(button1), std::ref(window));
 
 
     while(isRunning) {
